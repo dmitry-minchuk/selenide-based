093346c97ce712e0db25149a6d46be3295cd4556
dminchuk <dminchuk@exadel.com>
2020-03-05 14:05:21 +0300

    Adding retry logic (via testNG) and ability to set System properties on runtime meantime using config.properties as just default configuration

--- src/test/java/tests/cucumber/BaseCucumberTest.java
+++ src/test/java/tests/cucumber/BaseCucumberTest.java
@@ -2,14 +2,14 @@ package tests.cucumber;
 
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.logevents.SelenideLogger;
-import domain.PropertyNameSpace;
+import configuration.ProjectConfiguration;
+import configuration.domain.PropertyNameSpace;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.qameta.allure.selenide.AllureSelenide;
 import io.qameta.allure.selenide.LogType;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.testng.annotations.BeforeSuite;
-import utils.ProjectConfiguration;
 
 import java.util.logging.Level;
 
