feb73e9879c5647d332f517337dec70cdca7288e
Dmitry Minchuk <dzmitry.minchuk@connectwise.com>
2020-11-25 21:04:51 +0300

    Parallel tests

--- src/test/java/tests/testng/SampleTest.java
+++ src/test/java/tests/testng/SampleTest.java
@@ -6,9 +6,23 @@ import web.pages.GoogleHomePage;
 public class SampleTest extends BaseTest {
 
     @Test
-    public void homePageTest() {
+    public void homePageTest1() {
         GoogleHomePage googleHomePage = new GoogleHomePage();
         googleHomePage.open();
         googleHomePage.search("iphone");
+    }
+
+    @Test
+    public void homePageTest2() {
+        GoogleHomePage googleHomePage = new GoogleHomePage();
+        googleHomePage.open();
+        googleHomePage.search("samsung");
+    }
+
+    @Test
+    public void homePageTest3() {
+        GoogleHomePage googleHomePage = new GoogleHomePage();
+        googleHomePage.open();
+        googleHomePage.search("007");
     }
 }
