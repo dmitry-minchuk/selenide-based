54dbfd72af583547c7ba8128467a72c5c91a5957
Dmitry Minchuk <dzmitry.minchuk@connectwise.com>
2020-11-25 21:11:40 +0300

    more tests

--- src/test/java/tests/testng/SampleTest.java
+++ src/test/java/tests/testng/SampleTest.java
@@ -1,5 +1,6 @@
 package tests.testng;
 
+import com.codeborne.selenide.Selenide;
 import org.testng.annotations.Test;
 import web.pages.GoogleHomePage;
 
@@ -9,20 +10,224 @@ public class SampleTest extends BaseTest {
     public void homePageTest1() {
         GoogleHomePage googleHomePage = new GoogleHomePage();
         googleHomePage.open();
+        Selenide.sleep(60000);
         googleHomePage.search("iphone");
+        Selenide.sleep(60000);
     }
 
     @Test
     public void homePageTest2() {
         GoogleHomePage googleHomePage = new GoogleHomePage();
         googleHomePage.open();
-        googleHomePage.search("samsung");
+        Selenide.sleep(60000);
+        googleHomePage.search("iphone");
+        Selenide.sleep(60000);
     }
 
     @Test
     public void homePageTest3() {
         GoogleHomePage googleHomePage = new GoogleHomePage();
         googleHomePage.open();
-        googleHomePage.search("007");
+        Selenide.sleep(60000);
+        googleHomePage.search("iphone");
+        Selenide.sleep(60000);
+    }
+
+    @Test
+    public void homePageTest4() {
+        GoogleHomePage googleHomePage = new GoogleHomePage();
+        googleHomePage.open();
+        Selenide.sleep(60000);
+        googleHomePage.search("iphone");
+        Selenide.sleep(60000);
+    }
+
+    @Test
+    public void homePageTest5() {
+        GoogleHomePage googleHomePage = new GoogleHomePage();
+        googleHomePage.open();
+        Selenide.sleep(60000);
+        googleHomePage.search("iphone");
+        Selenide.sleep(60000);
+    }
+
+    @Test
+    public void homePageTest6() {
+        GoogleHomePage googleHomePage = new GoogleHomePage();
+        googleHomePage.open();
+        Selenide.sleep(60000);
+        googleHomePage.search("iphone");
+        Selenide.sleep(60000);
+    }
+
+    @Test
+    public void homePageTest7() {
+        GoogleHomePage googleHomePage = new GoogleHomePage();
+        googleHomePage.open();
+        Selenide.sleep(60000);
+        googleHomePage.search("iphone");
+        Selenide.sleep(60000);
+    }
+
+    @Test
+    public void homePageTest8() {
+        GoogleHomePage googleHomePage = new GoogleHomePage();
+        googleHomePage.open();
+        Selenide.sleep(60000);
+        googleHomePage.search("iphone");
+        Selenide.sleep(60000);
+    }
+
+    @Test
+    public void homePageTest9() {
+        GoogleHomePage googleHomePage = new GoogleHomePage();
+        googleHomePage.open();
+        Selenide.sleep(60000);
+        googleHomePage.search("iphone");
+        Selenide.sleep(60000);
+    }
+
+    @Test
+    public void homePageTest10() {
+        GoogleHomePage googleHomePage = new GoogleHomePage();
+        googleHomePage.open();
+        Selenide.sleep(60000);
+        googleHomePage.search("iphone");
+        Selenide.sleep(60000);
+    }
+
+    @Test
+    public void homePageTest11() {
+        GoogleHomePage googleHomePage = new GoogleHomePage();
+        googleHomePage.open();
+        Selenide.sleep(60000);
+        googleHomePage.search("iphone");
+        Selenide.sleep(60000);
+    }
+
+    @Test
+    public void homePageTest12() {
+        GoogleHomePage googleHomePage = new GoogleHomePage();
+        googleHomePage.open();
+        Selenide.sleep(60000);
+        googleHomePage.search("iphone");
+        Selenide.sleep(60000);
+    }
+
+    @Test
+    public void homePageTest13() {
+        GoogleHomePage googleHomePage = new GoogleHomePage();
+        googleHomePage.open();
+        Selenide.sleep(60000);
+        googleHomePage.search("iphone");
+        Selenide.sleep(60000);
+    }
+
+    @Test
+    public void homePageTest14() {
+        GoogleHomePage googleHomePage = new GoogleHomePage();
+        googleHomePage.open();
+        Selenide.sleep(60000);
+        googleHomePage.search("iphone");
+        Selenide.sleep(60000);
+    }
+
+    @Test
+    public void homePageTest15() {
+        GoogleHomePage googleHomePage = new GoogleHomePage();
+        googleHomePage.open();
+        Selenide.sleep(60000);
+        googleHomePage.search("iphone");
+        Selenide.sleep(60000);
+    }
+
+    @Test
+    public void homePageTest16() {
+        GoogleHomePage googleHomePage = new GoogleHomePage();
+        googleHomePage.open();
+        Selenide.sleep(60000);
+        googleHomePage.search("iphone");
+        Selenide.sleep(60000);
+    }
+
+    @Test
+    public void homePageTest17() {
+        GoogleHomePage googleHomePage = new GoogleHomePage();
+        googleHomePage.open();
+        Selenide.sleep(60000);
+        googleHomePage.search("iphone");
+        Selenide.sleep(60000);
+    }
+
+    @Test
+    public void homePageTest18() {
+        GoogleHomePage googleHomePage = new GoogleHomePage();
+        googleHomePage.open();
+        Selenide.sleep(60000);
+        googleHomePage.search("iphone");
+        Selenide.sleep(60000);
+    }
+
+    @Test
+    public void homePageTest19() {
+        GoogleHomePage googleHomePage = new GoogleHomePage();
+        googleHomePage.open();
+        Selenide.sleep(60000);
+        googleHomePage.search("iphone");
+        Selenide.sleep(60000);
+    }
+
+    @Test
+    public void homePageTest20() {
+        GoogleHomePage googleHomePage = new GoogleHomePage();
+        googleHomePage.open();
+        Selenide.sleep(60000);
+        googleHomePage.search("iphone");
+        Selenide.sleep(60000);
+    }
+
+    @Test
+    public void homePageTest21() {
+        GoogleHomePage googleHomePage = new GoogleHomePage();
+        googleHomePage.open();
+        Selenide.sleep(60000);
+        googleHomePage.search("iphone");
+        Selenide.sleep(60000);
+    }
+
+    @Test
+    public void homePageTest22() {
+        GoogleHomePage googleHomePage = new GoogleHomePage();
+        googleHomePage.open();
+        Selenide.sleep(60000);
+        googleHomePage.search("iphone");
+        Selenide.sleep(60000);
+    }
+
+    @Test
+    public void homePageTest23() {
+        GoogleHomePage googleHomePage = new GoogleHomePage();
+        googleHomePage.open();
+        Selenide.sleep(60000);
+        googleHomePage.search("iphone");
+        Selenide.sleep(60000);
+    }
+
+    @Test
+    public void homePageTest24() {
+        GoogleHomePage googleHomePage = new GoogleHomePage();
+        googleHomePage.open();
+        Selenide.sleep(60000);
+        googleHomePage.search("iphone");
+        Selenide.sleep(60000);
+    }
+
+    @Test
+    public void homePageTest25() {
+        GoogleHomePage googleHomePage = new GoogleHomePage();
+        googleHomePage.open();
+        Selenide.sleep(60000);
+        googleHomePage.search("iphone");
+        Selenide.sleep(60000);
     }
 }
